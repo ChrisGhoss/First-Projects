67062575286eecdf363bbb23f200a5026f99a559
ChrisGhoss <ghossoubchristopher@gmail.com>
2026-03-07 23:07:16 +0200

    changed the order (from < to >) for visibility while adding a != sign to not be too repetitive

--- christmas-ornament-estimator/test_ornament_calculator.py
+++ christmas-ornament-estimator/test_ornament_calculator.py
@@ -3,15 +3,15 @@ from ornament_calculator import ornament_estimator
 
 def test_budget():
     assert ornament_estimator("yes", 3, 1.2) != ornament_estimator("no", 3, 1.2)
-    assert ornament_estimator("yes", 5, 2) < ornament_estimator("no", 5, 2)
+    assert ornament_estimator("no", 5, 2) > ornament_estimator("yes", 5, 2)
 
 def test_height():
-    assert ornament_estimator("yes", 3, 1.2) < ornament_estimator("yes", 4, 1.2)
-    assert ornament_estimator("yes", 5, 2) < ornament_estimator("yes", 6, 2)
+    assert ornament_estimator("yes", 3, 1.2) != ornament_estimator("yes", 4, 1.2)
+    assert ornament_estimator("yes", 6, 2) > ornament_estimator("yes", 5, 2)
 
 def test_radius():
-    assert ornament_estimator("yes", 3, 1.2) < ornament_estimator("yes", 3, 2)
-    assert ornament_estimator("yes", 5, 2) < ornament_estimator("yes", 5, 3.5)
+    assert ornament_estimator("yes", 3, 1.2) != ornament_estimator("yes", 3, 2)
+    assert ornament_estimator("yes", 5, 3.5) > ornament_estimator("yes", 5, 2)
 
 def test_formula():
     assert ornament_estimator("yes", 3, 1.2) == 97
