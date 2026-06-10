ce4aeabb4d9069dd8642b39bcbf3baf959f96e74
ChrisGhoss <ghossoubchristopher@gmail.com>
2026-06-10 19:14:37 +0300

    Initial code for calculating the length of a table based on the amount of guests

---
+++ House Solutions/table-capacity/table_amount_solver.py
@@ -0,0 +1,34 @@
+def get_guests():
+    while True:
+        try:
+            amount = int(input("Enter the amount of guests for your table: "))
+            return amount
+        except ValueError():
+            print("Please enter a valid integer.")
+
+def yes_or_no(text):
+    print(text)
+    while True:
+        answer = input("Enter (yes/no): ").lower().strip()
+        if answer == "yes" or answer == "no":
+            return answer
+        print("Please type 'yes' or 'no'.")
+
+def seat_edges():
+    return yes_or_no("Do you want people sitting at the edges? ")
+
+def table_length():
+    space_per_person = 60
+    amount_of_guests = get_guests()
+    uses_seat_edges = seat_edges()
+    if uses_seat_edges == "no":
+        return (amount_of_guests / 2) * space_per_person
+    elif uses_seat_edges == "yes":
+        return ((amount_of_guests - 2) / 2) * space_per_person
+
+def main():
+    length = table_length()
+    print(f"Your table should be about {length}cm.")
+
+if __name__ == "__main__":
+    main()
