b5bb0a60f4abf6cec9aca9629ff4fa4c8068ca0b
ChrisGhoss <ghossoubchristopher@gmail.com>
2026-06-25 19:36:15 +0300

    Initial code for the program

---
+++ House Solutions/frame-spacing/frame_spacing_calculator.py
@@ -0,0 +1,43 @@
+def get_answer(text):
+    while True:
+        try:
+            value = int(input(text))
+            return value
+        except ValueError:
+            print("Please enter a valid integer.")
+
+def get_wall_width():
+    print("PS: Measure should be in centimeters (cm).")
+    return get_answer("Enter the width of your wall: ")
+
+def get_frame_number():
+    return get_answer("Enter the amount of frames: ")
+
+def yes_or_no(text):
+    print(text)
+    while True:
+        answer = input("Enter (yes/no): ").lower().strip()
+        if answer == "yes" or answer == "no":
+            return answer
+        print("Please type 'yes' or 'no'.")
+
+def get_frame_width(frame_number):
+    ans = yes_or_no("Are all your frames the same size? ")
+    if ans == "yes":
+        return get_answer("Enter the common width of your frame: ") * frame_number
+    elif ans == "no":
+        total = 0
+        for i in range(frame_number):
+            total += get_answer(f"Enter the width of frame #{i+1}: ")
+        return total
+    
+def main():
+    wall = get_wall_width()
+    amount = get_frame_number()
+    total_frame_width = get_frame_width(amount)
+    gaps = amount + 1
+    space = (wall - total_frame_width) / (gaps)
+    print(f"The amount of space in each gap should be about {space}cm.")
+
+if __name__ == "__main__":
+    main()
