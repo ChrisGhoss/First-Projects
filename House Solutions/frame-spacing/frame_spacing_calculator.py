f808003e586300a8fba5a7c5d13a850b2640da59
ChrisGhoss <ghossoubchristopher@gmail.com>
2026-06-25 20:17:51 +0300

    Added a summary

--- House Solutions/frame-spacing/frame_spacing_calculator.py
+++ House Solutions/frame-spacing/frame_spacing_calculator.py
@@ -34,10 +34,20 @@ def get_frame_width(frame_number):
             total += get_answer(f"Enter the width of frame #{i+1}: ", "float")
         return total
     
+def summary():
+    while True:
+        wall = get_wall_width()
+        amount = get_frame_number()
+        total_frame_width = get_frame_width(amount)
+        print("Summary:")
+        print(f"Wall Width: {wall}")
+        print(f"Frame Amount: {amount}")
+        print(f"Total Frame Width: {total_frame_width}")
+        if yes_or_no("Are you sure you want to continue?") == "yes":
+            return wall, amount, total_frame_width
+    
 def main():
-    wall = get_wall_width()
-    amount = get_frame_number()
-    total_frame_width = get_frame_width(amount)
+    wall, amount, total_frame_width = summary()
     gaps = amount + 1
     space = (wall - total_frame_width) / (gaps)
     print(f"The amount of space in each gap should be about {space}cm.")
