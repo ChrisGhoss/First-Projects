ad544a7ac49934fd9556e219f485266cad0f6acc
ChrisGhoss <ghossoubchristopher@gmail.com>
2026-08-12 21:47:52 +0300

    Final prototype before finishing

--- Game Solutions/poker-hand-ranker/hand_ranker.py
+++ Game Solutions/poker-hand-ranker/hand_ranker.py
@@ -1,8 +1,9 @@
+from collections import Counter
+
 def get_cards():
     cards = []
-    amount = int(input("How many cards do you want to rank? "))
     print ("PS: Cards should be written in the format '(number/letter + initial of suit)'.")
-    for i in range(amount):
+    for i in range(5):
         cards.append(input(f"Please enter card#{i+1}: ").upper())
     return cards
 
@@ -12,6 +13,23 @@ def seperate_value(cards):
     for card in cards:
         values.append(int(card[:-1]))
         suits.append(card[-1])
-    return values, suits
+    return sorted(values, reverse=True), suits
 
-print(seperate_value(get_cards()))
+def is_flush(l_suits):
+    for i in range(4):
+        if l_suits[i] != l_suits[i+1]:
+            return False
+    return True
+
+def is_straight(l_values):
+    for i in range(4):
+        if (l_values[i] - l_values[i+1]) != 1:
+            return False
+    return True
+
+def get_value_count(l_values):
+    counts = Counter(l_values)
+    return sorted(list(counts.values()), reverse=True)
+
+def main():
+    values, suits = seperate_value(get_cards())
